a254f131588743f11378266a384d850562d20104
Gabriel Guizardi <gabriel.guizardi.dev@gmail.com>
2025-07-10 20:24:07 -0300

    refactor: remove unused database serialization function

--- server/database.py
+++
@@ -1,5 +0,0 @@
-def serialize_objectid(obj):
-    """Convert ObjectId to string for JSON serialization."""
-    if isinstance(obj, ObjectId):
-        return str(obj)
-    raise TypeError(f"Object of type {obj.__class__.__name__} is not JSON serializable")
