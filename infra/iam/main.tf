e160e7fbda7433fbd6096e8753c1d89668df7ff4
Carlos Henrique dos Santos Pereira <carloshspereira@gmail.com>
2025-07-19 19:30:30 -0300

    Correção do IAM

--- infra/iam/main.tf
+++ infra/iam/main.tf
@@ -1,106 +1,19 @@
-resource "aws_iam_role" "iam_role" {
-  name = "iam-role-ipea-eia366-pbrent366"
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = "sts:AssumeRole"
-        Principal = {
-          Service = [
-            "events.amazonaws.com",
-            "states.amazonaws.com",
-            "glue.amazonaws.com",
-            "lambda.amazonaws.com",
-            "sagemaker.amazonaws.com"
-          ]
-        }
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
       }
-    ]
+    }]
   })
 }
 
-resource "aws_iam_policy" "iam_policy" {
-  name = "step-functions-policy-ipea-eia366-pbrent366"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "states:*",
-        ]
-        Resource = "*"
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "glue:*"
-        ]
-        Resource = "*"
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "sagemaker:CreateTrainingJob",
-          "sagemaker:DescribeTrainingJob",
-          "sagemaker:StopTrainingJob",
-          "sagemaker:AddTags"
-        ]
-        Resource = "*"
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "s3:*"
-        ]
-        Resource = "*"
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "iam:PassRole",
-        ]
-        Resource = "*"
-      },
-      {
-        Effect = "Allow",
-        Action = [
-          "ecr:*"
-        ],
-        Resource = "*"
-      },
-      {
-        Effect = "Allow",
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        Resource = "arn:aws:logs:*:*:*"
-      },
-      {
-        "Effect" : "Allow",
-        "Action" : [
-          "events:*"
-        ],
-        "Resource" : "*"
-      },
-      {
-        "Effect" : "Allow",
-        "Action" : [
-          "lambda:*"
-        ],
-        "Resource" : "*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_policy_attachment" "policy_attach" {
-  name       = "attach-policy"
-  roles      = [aws_iam_role.iam_role.name]
-  policy_arn = aws_iam_policy.iam_policy.arn
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
