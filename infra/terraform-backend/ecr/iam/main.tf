35181561210aa7933fd4245fda15018d9b152d5b
Carlos Henrique dos Santos Pereira <carloshspereira@gmail.com>
2025-07-20 15:41:17 -0300

    Removendo region da criação das roles de backend

--- infra/terraform-backend/ecr/iam/main.tf
+++ infra/terraform-backend/ecr/iam/main.tf
@@ -3,7 +3,6 @@ resource "aws_iam_role" "role" {
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    region = var.region
     Statement = [
       {
         Action = "sts:AssumeRole",
