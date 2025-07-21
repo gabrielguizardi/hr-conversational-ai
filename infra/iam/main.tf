ae15efe357548fcf6aa2b0b0a2fc4f22c5257a96
Carlos Henrique dos Santos Pereira <carloshspereira@gmail.com>
2025-07-20 23:40:33 -0300

    Nome duplicado

--- infra/iam/main.tf
+++ infra/iam/main.tf
@@ -19,7 +19,7 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
 }
 
 # IAM para executar tasks
-resource "aws_iam_role" "ecs_task_execution_role" {
+resource "aws_iam_role" "ecs_task_execution_frontend_role" {
   name = "${var.frontend_app_name}-ecs-role"
 
   assume_role_policy = jsonencode({
@@ -33,6 +33,6 @@ resource "aws_iam_role" "ecs_task_execution_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_policy" {
-  role       = aws_iam_role.ecs_task_execution_role.name
+  role       = aws_iam_role.ecs_task_execution_frontend_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
